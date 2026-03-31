597cdc2eb23024a25ada2906a2bb024439139e72
chetan <chetan.mali@jbdevs.xyz>
2026-03-31 17:58:28 +0530

    add category apis

--- productModule/serializers.py
+++ productModule/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import ProductModule
+from .models import ProductModule, Category
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductModule
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
         fields = '__all__'
